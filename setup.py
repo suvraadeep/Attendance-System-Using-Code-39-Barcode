e105b27d70e7c1ee87c3d6d4a49a5237542d5037
suvraadeep <dassuvradeep9@gmail.com>
2024-01-14 15:08:23 +0530

    updated

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='suvradeep',
     author_email='dassuvradeep9@gmail.com',
-    install_requires=["opencv-contrib-python==4.7.0.72","opencv-python==4.8.0.74","opencv-python-headless==4.8.0.74","pythoncom","pyzbar","win32com.client","tkinter","pandas","openpyx"],
+    install_requires=["opencv-contrib-python==4.7.0.72","opencv-python==4.8.0.74","opencv-python-headless==4.8.0.74","pythoncom","pyzbar","win32com.client","tkinter","pandas","openpyx","numpy"],
     packages=find_packages()
 )
